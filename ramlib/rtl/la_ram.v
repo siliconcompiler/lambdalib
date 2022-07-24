fdb83f11df73bf31e7c05d6638ae6bac2c1be774
aolofsson <andreas@zeroasic.com>
2022-07-24 13:51:38 -0400

    Making ram interface more traditional

--- ramlib/rtl/la_ram.v
+++ ramlib/rtl/la_ram.v
@@ -21,40 +21,40 @@
  ****************************************************************************/
 
 module la_ram
-  #(parameter N         = 32,            // Memory width
-    parameter DEPTH     = 32,            // Memory depth
-    parameter TYPE      = "DEFAULT",     // pass through variable for hard macro
-    parameter REG       = 1,             // adds pipeline stage to RAM
-    parameter AW        = $clog2(DEPTH), // address width (derived)
-    parameter CFGWIDTH  = 128,           // width of config interface
-    parameter TESTWIDTH = 128            // width of test interface
+  #(parameter N      = 32,            // Memory width
+    parameter DEPTH  = 32,            // Memory depth
+    parameter TYPE   = "DEFAULT",     // pass through variable for hard macro
+    parameter REG    = 1,             // adds pipeline stage to RAM
+    parameter AW     = $clog2(DEPTH), // address width (derived)
+    parameter CFGW   = 128,           // width of config interface
+    parameter TESTW  = 128            // width of test interface
     )
    (// Memory interface (dual port)
-    input 		  clk, // write clock
-    input 		  en, // chip enable
-    input 		  we, // write enable
-    input [N-1:0] 	  wem, //per bit write mask
-    input [AW-1:0] 	  addr,//write address
-    input [N-1:0] 	  din, //write data
-    output [N-1:0] 	  dout,//read output data
+    input 	      clk, // write clock
+    input 	      ce, // chip enable
+    input 	      we, // write enable
+    input [N-1:0]     wmask, //per bit write mask
+    input [AW-1:0]    addr,//write address
+    input [N-1:0]     din, //write data
+    output [N-1:0]    dout,//read output data
     // Power signals
-    input 		  vss, // ground signal
-    input 		  vdd, // memory core array power
-    input 		  vddio, // periphery/io power
+    input 	      vss, // ground signal
+    input 	      vdd, // memory core array power
+    input 	      vddio, // periphery/io power
     // Generic interfaces
-    input [CFGWIDTH-1:0]  cfg, // generic config/test interface
-    input [TESTWIDTH-1:0] test // generic test interface
+    input [CFGW-1:0]  cfg, // generic config/test interface
+    input [TESTW-1:0] test // generic test interface
     );
 
    // Generic RTL RAM
-   reg [N-1:0] 		  ram    [0:DEPTH-1];
-   integer 		  i;
-   wire [N-1:0] 	  rdata;
+   reg [N-1:0] 	       ram    [0:DEPTH-1];
+   integer 	       i;
+   wire [N-1:0]        rdata;
 
    //write port
    always @(posedge clk)
      for (i=0;i<N;i=i+1)
-       if (en & wem[i])
+       if (ce & we & wmask[i])
          ram[addr[AW-1:0]][i] <= din[i];
 
    //read port
@@ -63,7 +63,7 @@ module la_ram
    //configurable output register
    reg [N-1:0] 		  rd_reg;
    always @ (posedge clk)
-     if(en)
+     if(ce)
        rd_reg[N-1:0] <= rdata[N-1:0];
 
    //Drive output from register or RAM directly
