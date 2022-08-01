fd247f61cbbd5e49bf73532b15a289f0bc89c1ce
aolofsson <andreas@zeroasic.com>
2022-07-31 21:56:03 -0400

    Testbench looks good now - Fixing nasty overflow bug in concatenation

--- iolib/testbench/tb_la_iopadring.v
+++ iolib/testbench/tb_la_iopadring.v
@@ -1,59 +1,63 @@
 module testbench();
 
-   parameter [4:0] CFGW  = 8;
-   parameter [4:0] RINGW = 8;
-   parameter [0:0] ENCUT = 1;
-   parameter [0:0] ENPOC = 1;
+   parameter [7:0] CFGW  = 8'h8;
+   parameter [7:0] RINGW = 8'h8;
+   parameter [0:0] ENCUT = 1'h1;
+   parameter [0:0] ENPOC = 1'h1;
 
-   parameter [4:0] NO_SECTIONS   =  1; // north
-   parameter [255:0] NO_NSIDE    =  16;
-   parameter [255:0] NO_NGPIO    =  16;
-   parameter [255:0] NO_NANALOG  =  0;
-   parameter [255:0] NO_NXTAL    =  0;
-   parameter [255:0] NO_NVDDIO   =  1;
-   parameter [255:0] NO_NVDD     =  1;
-   parameter [255:0] NO_NGND     =  1;
-   parameter [255:0] NO_NCLAMP   =  0;
-   parameter [4:0] EA_SECTIONS   =  1; // east
-   parameter [255:0] EA_NSIDE    =  16;
-   parameter [255:0] EA_NGPIO    =  16;
-   parameter [255:0] EA_NANALOG  =  0;
-   parameter [255:0] EA_NXTAL    =  0;
-   parameter [255:0] EA_NVDDIO   =  1;
-   parameter [255:0] EA_NVDD     =  1;
-   parameter [255:0] EA_NGND     =  1;
-   parameter [255:0] EA_NCLAMP   =  0;
-   parameter [4:0] SO_SECTIONS   =  1; // south
-   parameter [255:0] SO_NSIDE    =  16;
-   parameter [255:0] SO_NGPIO    =  16;
-   parameter [255:0] SO_NANALOG  =  0;
-   parameter [255:0] SO_NXTAL    =  0;
-   parameter [255:0] SO_NVDDIO   =  1;
-   parameter [255:0] SO_NVDD     =  1;
-   parameter [255:0] SO_NGND     =  1;
-   parameter [255:0] SO_NCLAMP   =  0;
-   parameter [4:0] WE_SECTIONS   =  1; // west
-   parameter [255:0] WE_NSIDE    =  16;
-   parameter [255:0] WE_NGPIO    =  16;
-   parameter [255:0] WE_NANALOG  =  0;
-   parameter [255:0] WE_NXTAL    =  0;
-   parameter [255:0] WE_NVDDIO   =  1;
-   parameter [255:0] WE_NVDD     =  1;
-   parameter [255:0] WE_NGND     =  1;
-   parameter [255:0] WE_NCLAMP   =  0;
+   parameter [4:0]  NO_SECTIONS =  2; // north
+   parameter [7:0]  NO_NSIDE    =  8'h8;
+   parameter [63:0] NO_NGPIO    =  64'h8040201008040201;
+   parameter [63:0] NO_NANALOG  =  64'h0000000001000201;
+   parameter [63:0] NO_NXTAL    =  64'h0000000001020001;
+   parameter [63:0] NO_NVDDIO   =  64'h0807060504030201;
+   parameter [63:0] NO_NVDD     =  64'h0807060504030201;
+   parameter [63:0] NO_NGND     =  64'h0807060504030201;
+   parameter [63:0] NO_NCLAMP   =  64'h0807060504030201;
+   parameter [4:0]  EA_SECTIONS =  2; // north
+   parameter [7:0]  EA_NSIDE    =  8'h10;
+   parameter [63:0] EA_NGPIO    =  64'h8040201008040201;
+   parameter [63:0] EA_NANALOG  =  64'h0000000001000201;
+   parameter [63:0] EA_NXTAL    =  64'h0000000001020001;
+   parameter [63:0] EA_NVDDIO   =  64'h0807060504030201;
+   parameter [63:0] EA_NVDD     =  64'h0807060504030201;
+   parameter [63:0] EA_NGND     =  64'h0807060504030201;
+   parameter [63:0] EA_NCLAMP   =  64'h0807060504030201;
+   parameter [4:0]  SO_SECTIONS =  2; // north
+   parameter [7:0]  SO_NSIDE    =  8'h20;
+   parameter [63:0] SO_NGPIO    =  64'h8040201008040201;
+   parameter [63:0] SO_NANALOG  =  64'h0000000001000201;
+   parameter [63:0] SO_NXTAL    =  64'h0000000001020001;
+   parameter [63:0] SO_NVDDIO   =  64'h0807060504030201;
+   parameter [63:0] SO_NVDD     =  64'h0807060504030201;
+   parameter [63:0] SO_NGND     =  64'h0807060504030201;
+   parameter [63:0] SO_NCLAMP   =  64'h0807060504030201;
+   parameter [4:0]  WE_SECTIONS =  2; // north
+   parameter [7:0]  WE_NSIDE    =  8'h40;
+   parameter [63:0] WE_NGPIO    =  64'h8040201008040201;
+   parameter [63:0] WE_NANALOG  =  64'h0000000001000201;
+   parameter [63:0] WE_NXTAL    =  64'h0000000001020001;
+   parameter [63:0] WE_NVDDIO   =  64'h0807060504030201;
+   parameter [63:0] WE_NVDD     =  64'h0807060504030201;
+   parameter [63:0] WE_NGND     =  64'h0807060504030201;
+   parameter [63:0] WE_NCLAMP   =  64'h0807060504030201;
 
-   //local wires
+   // local wires
    wire [EA_NSIDE-1:0] ea_z;			// From dut of la_iopadring.v
    wire [NO_NSIDE-1:0] no_z;			// From dut of la_iopadring.v
    wire [SO_NSIDE-1:0] so_z;			// From dut of la_iopadring.v
    wire [WE_NSIDE-1:0] we_z;			// From dut of la_iopadring.v
    wire 	       vss;			// To/From dut of la_iopadring.v
+   wire 	       dut_fail;		// To oh_simctrl of oh_simctrl.v
+   wire 	       dut_done;
+
+   // local params (needed to avoid overflow in concat)
+   localparam [15:0] NO_CFGW = NO_NSIDE * CFGW;
+   localparam [15:0] EA_CFGW = EA_NSIDE * CFGW;
+   localparam [15:0] SO_CFGW = SO_NSIDE * CFGW;
+   localparam [15:0] WE_CFGW = WE_NSIDE * CFGW;
 
    /*AUTOINPUT*/
-   // Beginning of automatic inputs (from unused autoinst inputs)
-   input		dut_done;		// To oh_simctrl of oh_simctrl.v
-   input		dut_fail;		// To oh_simctrl of oh_simctrl.v
-   // End of automatics
    /*AUTOWIRE*/
    // Beginning of automatic wires (for undeclared instantiated-module outputs)
    wire			clk;			// From oh_simctrl of oh_simctrl.v
@@ -105,22 +109,23 @@ module testbench();
     .\(.*\)_vdd	 (\1_vdd[]),
     .\(.*\)_vddio(\1_vddio[]),
     .\(.*\)_vssio(\1_vssdio[]),
-    .no_\(.*\)	 ({(NO_NSIDE){1'b0}}),
-    .no_cfg	 ({(NO_NSIDE*CFGW){1'b0}}),
-    .no_ds	 ({(3*NO_NSIDE){1'b0}}),
-    .ea_\(.*\)	 ({(EA_NSIDE){1'b0}}),
-    .ea_cfg	 ({(EA_NSIDE*CFGW){1'b0}}),
-    .ea_ds	 ({(3*EA_NSIDE){1'b0}}),
-    .so_\(.*\)	 ({(SO_NSIDE){1'b0}}),
-    .so_cfg	 ({(SO_NSIDE*CFGW){1'b0}}),
-    .so_ds	 ({(3*SO_NSIDE){1'b0}}),
-    .we_\(.*\)	 ({(WE_NSIDE){1'b0}}),
-    .we_cfg	 ({(WE_NSIDE*CFGW){1'b0}}),
-    .we_ds	 ({(3*WE_NSIDE){1'b0}}),
+    .no_\(.*\)	 ({NO_NSIDE{1'b0}}),
+    .no_cfg	 ({NO_CFGW{1'b0}}),
+    .no_ds	 ({3*NO_NSIDE{1'b0}}),
+    .ea_\(.*\)	 ({EA_NSIDE{1'b0}}),
+    .ea_cfg	 ({EA_CFGW{1'b0}}),
+    .ea_ds	 ({3*EA_NSIDE{1'b0}}),
+    .so_\(.*\)	 ({SO_NSIDE{1'b0}}),
+    .so_cfg	 ({SO_CFGW{1'b0}}),
+    .so_ds	 ({3*SO_NSIDE{1'b0}}),
+    .we_\(.*\)	 ({WE_NSIDE{1'b0}}),
+    .we_cfg	 ({WE_CFGW{1'b0}}),
+    .we_ds	 ({3*WE_NSIDE{1'b0}}),
     );
     */
 
    la_iopadring #(// north
+		  .NO_SECTIONS(NO_SECTIONS),
 		  .NO_NSIDE(NO_NSIDE),
 		  .NO_NGPIO(NO_NGPIO),
 		  .NO_NANALOG(NO_NANALOG),
@@ -130,6 +135,7 @@ module testbench();
 		  .NO_NGND(NO_NGND),
 		  .NO_NCLAMP(NO_NCLAMP),
 		  // east
+		  .EA_SECTIONS(EA_SECTIONS),
 		  .EA_NSIDE(EA_NSIDE),
 		  .EA_NGPIO(EA_NGPIO),
 		  .EA_NANALOG(EA_NANALOG),
@@ -139,6 +145,7 @@ module testbench();
 		  .EA_NGND(EA_NGND),
 		  .EA_NCLAMP(EA_NCLAMP),
 		  // south
+		  .SO_SECTIONS(SO_SECTIONS),
 		  .SO_NSIDE(SO_NSIDE),
 		  .SO_NGPIO(SO_NGPIO),
 		  .SO_NANALOG(SO_NANALOG),
@@ -148,6 +155,7 @@ module testbench();
 		  .SO_NGND(SO_NGND),
 		  .SO_NCLAMP(SO_NCLAMP),
 		  // west
+		  .WE_SECTIONS(WE_SECTIONS),
 		  .WE_NSIDE(WE_NSIDE),
 		  .WE_NGPIO(WE_NGPIO),
 		  .WE_NANALOG(WE_NANALOG),
@@ -161,67 +169,69 @@ module testbench();
 		  .RINGW(RINGW),
 		  .ENPOC(ENPOC),
 		  .ENCUT(ENCUT))
-     dut (/*AUTOINST*/
-	  // Outputs
-	  .no_z				(no_z[NO_NSIDE-1:0]),	 // Templated
-	  .ea_z				(ea_z[EA_NSIDE-1:0]),	 // Templated
-	  .so_z				(so_z[SO_NSIDE-1:0]),	 // Templated
-	  .we_z				(we_z[WE_NSIDE-1:0]),	 // Templated
-	  // Inouts
-	  .vss				(vss),
-	  .no_pad			(no_pad[NO_NSIDE-1:0]),	 // Templated
-	  .no_vdd			(no_vdd[NO_SECTIONS-1:0]), // Templated
-	  .no_vddio			(no_vddio[NO_SECTIONS-1:0]), // Templated
-	  .no_vssio			(no_vssdio[NO_SECTIONS-1:0]), // Templated
-	  .ea_pad			(ea_pad[EA_NSIDE-1:0]),	 // Templated
-	  .ea_vdd			(ea_vdd[EA_SECTIONS-1:0]), // Templated
-	  .ea_vddio			(ea_vddio[EA_SECTIONS-1:0]), // Templated
-	  .ea_vssio			(ea_vssdio[EA_SECTIONS-1:0]), // Templated
-	  .so_pad			(so_pad[SO_NSIDE-1:0]),	 // Templated
-	  .so_vdd			(so_vdd[SO_SECTIONS-1:0]), // Templated
-	  .so_vddio			(so_vddio[SO_SECTIONS-1:0]), // Templated
-	  .so_vssio			(so_vssdio[SO_SECTIONS-1:0]), // Templated
-	  .we_pad			(we_pad[WE_NSIDE-1:0]),	 // Templated
-	  .we_vdd			(we_vdd[WE_SECTIONS-1:0]), // Templated
-	  .we_vddio			(we_vddio[WE_SECTIONS-1:0]), // Templated
-	  .we_vssio			(we_vssdio[WE_SECTIONS-1:0]), // Templated
-	  // Inputs
-	  .no_a				({(NO_NSIDE){1'b0}}),	 // Templated
-	  .no_ie			({(NO_NSIDE){1'b0}}),	 // Templated
-	  .no_oe			({(NO_NSIDE){1'b0}}),	 // Templated
-	  .no_pe			({(NO_NSIDE){1'b0}}),	 // Templated
-	  .no_ps			({(NO_NSIDE){1'b0}}),	 // Templated
-	  .no_sr			({(NO_NSIDE){1'b0}}),	 // Templated
-	  .no_st			({(NO_NSIDE){1'b0}}),	 // Templated
-	  .no_ds			({(3*NO_NSIDE){1'b0}}),	 // Templated
-	  .no_cfg			({(NO_NSIDE*CFGW){1'b0}}), // Templated
-	  .ea_a				({(EA_NSIDE){1'b0}}),	 // Templated
-	  .ea_ie			({(EA_NSIDE){1'b0}}),	 // Templated
-	  .ea_oe			({(EA_NSIDE){1'b0}}),	 // Templated
-	  .ea_pe			({(EA_NSIDE){1'b0}}),	 // Templated
-	  .ea_ps			({(EA_NSIDE){1'b0}}),	 // Templated
-	  .ea_sr			({(EA_NSIDE){1'b0}}),	 // Templated
-	  .ea_st			({(EA_NSIDE){1'b0}}),	 // Templated
-	  .ea_ds			({(3*EA_NSIDE){1'b0}}),	 // Templated
-	  .ea_cfg			({(EA_NSIDE*CFGW){1'b0}}), // Templated
-	  .so_a				({(SO_NSIDE){1'b0}}),	 // Templated
-	  .so_ie			({(SO_NSIDE){1'b0}}),	 // Templated
-	  .so_oe			({(SO_NSIDE){1'b0}}),	 // Templated
-	  .so_pe			({(SO_NSIDE){1'b0}}),	 // Templated
-	  .so_ps			({(SO_NSIDE){1'b0}}),	 // Templated
-	  .so_sr			({(SO_NSIDE){1'b0}}),	 // Templated
-	  .so_st			({(SO_NSIDE){1'b0}}),	 // Templated
-	  .so_ds			({(3*SO_NSIDE){1'b0}}),	 // Templated
-	  .so_cfg			({(SO_NSIDE*CFGW){1'b0}}), // Templated
-	  .we_a				({(WE_NSIDE){1'b0}}),	 // Templated
-	  .we_ie			({(WE_NSIDE){1'b0}}),	 // Templated
-	  .we_oe			({(WE_NSIDE){1'b0}}),	 // Templated
-	  .we_pe			({(WE_NSIDE){1'b0}}),	 // Templated
-	  .we_ps			({(WE_NSIDE){1'b0}}),	 // Templated
-	  .we_sr			({(WE_NSIDE){1'b0}}),	 // Templated
-	  .we_st			({(WE_NSIDE){1'b0}}),	 // Templated
-	  .we_ds			({(3*WE_NSIDE){1'b0}}),	 // Templated
-	  .we_cfg			({(WE_NSIDE*CFGW){1'b0}})); // Templated
+   dut (/*AUTOINST*/
+	// Outputs
+	.no_z				(no_z[NO_NSIDE-1:0]),	 // Templated
+	.ea_z				(ea_z[EA_NSIDE-1:0]),	 // Templated
+	.so_z				(so_z[SO_NSIDE-1:0]),	 // Templated
+	.we_z				(we_z[WE_NSIDE-1:0]),	 // Templated
+	// Inouts
+	.vss				(vss),
+	.no_pad				(no_pad[NO_NSIDE-1:0]),	 // Templated
+	.no_vdd				(no_vdd[NO_SECTIONS-1:0]), // Templated
+	.no_vddio			(no_vddio[NO_SECTIONS-1:0]), // Templated
+	.no_vssio			(no_vssdio[NO_SECTIONS-1:0]), // Templated
+	.ea_pad				(ea_pad[EA_NSIDE-1:0]),	 // Templated
+	.ea_vdd				(ea_vdd[EA_SECTIONS-1:0]), // Templated
+	.ea_vddio			(ea_vddio[EA_SECTIONS-1:0]), // Templated
+	.ea_vssio			(ea_vssdio[EA_SECTIONS-1:0]), // Templated
+	.so_pad				(so_pad[SO_NSIDE-1:0]),	 // Templated
+	.so_vdd				(so_vdd[SO_SECTIONS-1:0]), // Templated
+	.so_vddio			(so_vddio[SO_SECTIONS-1:0]), // Templated
+	.so_vssio			(so_vssdio[SO_SECTIONS-1:0]), // Templated
+	.we_pad				(we_pad[WE_NSIDE-1:0]),	 // Templated
+	.we_vdd				(we_vdd[WE_SECTIONS-1:0]), // Templated
+	.we_vddio			(we_vddio[WE_SECTIONS-1:0]), // Templated
+	.we_vssio			(we_vssdio[WE_SECTIONS-1:0]), // Templated
+	// Inputs
+	.no_a				({NO_NSIDE{1'b0}}),	 // Templated
+	.no_ie				({NO_NSIDE{1'b0}}),	 // Templated
+	.no_oe				({NO_NSIDE{1'b0}}),	 // Templated
+	.no_pe				({NO_NSIDE{1'b0}}),	 // Templated
+	.no_ps				({NO_NSIDE{1'b0}}),	 // Templated
+	.no_sr				({NO_NSIDE{1'b0}}),	 // Templated
+	.no_st				({NO_NSIDE{1'b0}}),	 // Templated
+	.no_ds				({3*NO_NSIDE{1'b0}}),	 // Templated
+	.no_cfg				({NO_CFGW{1'b0}}),	 // Templated
+	.ea_a				({EA_NSIDE{1'b0}}),	 // Templated
+	.ea_ie				({EA_NSIDE{1'b0}}),	 // Templated
+	.ea_oe				({EA_NSIDE{1'b0}}),	 // Templated
+	.ea_pe				({EA_NSIDE{1'b0}}),	 // Templated
+	.ea_ps				({EA_NSIDE{1'b0}}),	 // Templated
+	.ea_sr				({EA_NSIDE{1'b0}}),	 // Templated
+	.ea_st				({EA_NSIDE{1'b0}}),	 // Templated
+	.ea_ds				({3*EA_NSIDE{1'b0}}),	 // Templated
+	.ea_cfg				({EA_CFGW{1'b0}}),	 // Templated
+	.so_a				({SO_NSIDE{1'b0}}),	 // Templated
+	.so_ie				({SO_NSIDE{1'b0}}),	 // Templated
+	.so_oe				({SO_NSIDE{1'b0}}),	 // Templated
+	.so_pe				({SO_NSIDE{1'b0}}),	 // Templated
+	.so_ps				({SO_NSIDE{1'b0}}),	 // Templated
+	.so_sr				({SO_NSIDE{1'b0}}),	 // Templated
+	.so_st				({SO_NSIDE{1'b0}}),	 // Templated
+	.so_ds				({3*SO_NSIDE{1'b0}}),	 // Templated
+	.so_cfg				({SO_CFGW{1'b0}}),	 // Templated
+	.we_a				({WE_NSIDE{1'b0}}),	 // Templated
+	.we_ie				({WE_NSIDE{1'b0}}),	 // Templated
+	.we_oe				({WE_NSIDE{1'b0}}),	 // Templated
+	.we_pe				({WE_NSIDE{1'b0}}),	 // Templated
+	.we_ps				({WE_NSIDE{1'b0}}),	 // Templated
+	.we_sr				({WE_NSIDE{1'b0}}),	 // Templated
+	.we_st				({WE_NSIDE{1'b0}}),	 // Templated
+	.we_ds				({3*WE_NSIDE{1'b0}}),	 // Templated
+	.we_cfg				({WE_CFGW{1'b0}}));	 // Templated
+
+
 
 
 
