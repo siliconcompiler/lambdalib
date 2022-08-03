00d57fc3fd2d91e4941635e84fca8ec4735f3ede
aolofsson <andreas@zeroasic.com>
2022-08-03 18:58:57 -0400

    Updating floorplan script for new directory structure

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lambda
+
 Standardized ASIC design PDKs
+
+
+### Update submodules
+
+```
+cd submodules/openpdks
+git checkout master
+git pull
+cd ../../
+git commit -am "Updated to latest submodule"
+```
--- padring/floorplan/la_iopadring.py
+++ padring/floorplan/la_iopadring.py
@@ -9,9 +9,10 @@ topmodule = "la_iopadring"
 chip = siliconcompiler.Chip(topmodule)
 chip.load_target('skywater130_demo')
 
-chip.set('input', 'verilog', f'{root}/../rtl/la_iopadring.v')
-chip.add('option', 'ydir', f'{root}/../padring/rtl/')
-chip.add('option', 'ydir', 'iolib/adapter/sky130io/rtl/')
+chip.set('input', 'verilog', f'{root}/padring/rtl/la_iopadring.v')
+chip.add('option', 'ydir', f'{root}/padring/rtl/')
+chip.add('option', 'ydir',  f'{root}/submodules/openpdks/pdks/sky130/libs/sky130io/lambda')
+chip.add('option', 'ydir',  f'{root}/submodules/openpdks/pdks/sky130/libs/sky130io/bb')
 chip.set('option', 'relax', True)
 chip.set('option', 'steplist', ['import', 'syn'])
 chip.run()
