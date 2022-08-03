447a44ba2d5264571763333d66eec1273b317391
aolofsson <andreas@zeroasic.com>
2022-08-03 12:11:14 -0400

    Removing old git repo

--- padring/floorplan/la_iopadring.py
+++ padring/floorplan/la_iopadring.py
@@ -4,10 +4,9 @@ import siliconcompiler
 
 topmodule = "la_iopadring"
 chip = siliconcompiler.Chip(topmodule)
-chip.load_target('skywater130_demo')
+chip.load_target('ramp_gf12lp')
 
 chip.set('input', 'verilog', 'padring/rtl/la_iopadring.v')
-chip.add('input', 'verilog', 'skywater/skywater130/libs/sky130io/v0_0_2/io/sky130_io.blackbox.v')
 chip.add('option', 'ydir', 'padring/rtl/')
 chip.add('option', 'ydir', 'iolib/adapter/sky130io/rtl/')
 chip.set('option', 'relax', True)
