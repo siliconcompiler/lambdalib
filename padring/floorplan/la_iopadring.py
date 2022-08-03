f8b803c3c6bf816fe905dabd0fb0d609e2d40955
aolofsson <andreas@zeroasic.com>
2022-08-03 18:43:38 -0400

    Reorg to make it clarify submodules

--- padring/floorplan/la_iopadring.py
+++ padring/floorplan/la_iopadring.py
@@ -2,12 +2,15 @@ import os
 import re
 import siliconcompiler
 
+
+root = os.path.dirname(__file__) + "../../"
+
 topmodule = "la_iopadring"
 chip = siliconcompiler.Chip(topmodule)
-chip.load_target('ramp_gf12lp')
+chip.load_target('skywater130_demo')
 
-chip.set('input', 'verilog', 'padring/rtl/la_iopadring.v')
-chip.add('option', 'ydir', 'padring/rtl/')
+chip.set('input', 'verilog', f'{root}/../rtl/la_iopadring.v')
+chip.add('option', 'ydir', f'{root}/../padring/rtl/')
 chip.add('option', 'ydir', 'iolib/adapter/sky130io/rtl/')
 chip.set('option', 'relax', True)
 chip.set('option', 'steplist', ['import', 'syn'])
