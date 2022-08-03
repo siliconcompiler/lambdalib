0276cc0b900b802253e8005cf59ca6a26925cfb1
aolofsson <andreas@zeroasic.com>
2022-08-02 21:44:42 -0400

    Work in progress build script (reorganizing dirs)

--- padring/floorplan/la_iopadring.py
+++ padring/floorplan/la_iopadring.py
@@ -6,9 +6,10 @@ topmodule = "la_iopadring"
 chip = siliconcompiler.Chip(topmodule)
 chip.load_target('skywater130_demo')
 
-
-chip.set('input', 'verilog', '../rtl/la_iopadring.v')
-chip.add('option', 'ydir', '../rtl/')
-chip.add('option', 'ydir', '../../iolib/adapter/sky130io/rtl')
+chip.set('input', 'verilog', 'padring/rtl/la_iopadring.v')
+chip.add('input', 'verilog', 'skywater/skywater130/libs/sky130io/v0_0_2/io/sky130_io.blackbox.v')
+chip.add('option', 'ydir', 'padring/rtl/')
+chip.add('option', 'ydir', 'iolib/adapter/sky130io/rtl/')
+chip.set('option', 'relax', True)
 chip.set('option', 'steplist', ['import', 'syn'])
 chip.run()
