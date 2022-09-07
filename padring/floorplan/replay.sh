e274afd958955651c89fde0c64c120dcbfd4a0e7
aolofsson <andreas@zeroasic.com>
2022-09-06 22:26:41 -0400

    Revert "git submodule updated"
    This reverts commit 15694bcf4d0dca2f3087c55deb987993dbb13065.

--- padring/floorplan/replay.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-surelog -elabuhdm -parse ../rtl/la_iopadring.v ../rtl/la_ioside.v ../rtl/la_iosection.v  -y ../../iolib/adapter/sky130io/rtl -top la_iopadring +libext+.sv+.v -timescale=1ns/1ps
