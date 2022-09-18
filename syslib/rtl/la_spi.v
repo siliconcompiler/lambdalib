d7c5146e465f07037bdae69b6e6d176065522e40
aolofsson <andreas@zeroasic.com>
2022-09-17 22:52:59 -0400

    Changing away from mosi/miso naming - Making selection dynami

--- syslib/rtl/la_spi.v
+++ syslib/rtl/la_spi.v
@@ -8,55 +8,38 @@
  ****************************************************************************/
 module la_spi
   #(
-    parameter TYPE = "HOST", // interface selection
-    parameter PW   = 256,    // packet width
-    parameter CTRLW = 8,     // control vector width
-    parameter STATW = 8      // status vector width
+    parameter TYPE    = "HOST", // interface selection
+    parameter UW      = 256,    // packet width
+    parameter CTRLW   = 8,      // control vector width
+    parameter STATUSW = 8       // status vector width
     )
    (// basic control signals
     input 	       clk, // core clock
     input 	       nreset, // active low async reset
+    input 	       hostmode, // 1=hostmode
     input [CTRLW-1:0]  ctrl, // free form ctrl inputs
     output [STATW-1:0] status, // free form status outputs
-    // common memory mapped core interface
-    input [PW-1:0]     tx_data,
-    input 	       tx_valid,
-    output 	       tx_ready,
-    output [PW-1:0]    rx_data,
-    output 	       rx_valid,
-    input 	       rx_ready,
-    // inputs
-    input 	       spi_sclk_in,
-    input 	       spi_csn_in,
-    input 	       spi_mosi_in,
-    input 	       spi_miso_in,
-    // outputs
-    output 	       spi_sclk_out,
+    // umi interface
+    input 	       valid_in,
+    input [UW-1:0]     packet_in,
+    output 	       ready_out,
+    output 	       valid_out,
+    output [UW-1:0]    packet_out,
+    input 	       ready_in
+    // io interface
+    input 	       spi_sck_in, // serial clock
+    output 	       spi_sck_out,
+    output 	       spi_sck_oe,
+    input 	       spi_csn_in, // chip select (active low)
     output 	       spi_csn_out,
-    output 	       spi_mosi_out,
-    output 	       spi_miso_out,
-    // output enable
-    output 	       spi_sclk_oe,
     output 	       spi_csn_oe,
-    output 	       spi_mosi_oe,
-    output 	       spi_miso_oe
+    input 	       spi_sd_in, // serial data
+    output 	       spi_sd_out,
+    output 	       spi_sd_oe
     );
 
-   generate
-      if (TYPE=="HOST")
-	begin
-	   assign spi_sclk_oe = 1'b1;
-	   assign spi_csn_oe = 1'b1;
-	   assign spi_mosi_oe = 1'b1;
-	   assign spi_miso_oe = 1'b0;
-	end
-      else
-	begin
-	   assign spi_sclk_oe = 1'b0;
-	   assign spi_csn_oe = 1'b0;
-	   assign spi_mosi_oe = 1'b0;
-	   assign spi_miso_oe = 1'b1;
-	end
-   endgenerate
+   assign spi_sck_oe = hostmode ? 1'b1 : 1'b0;
+   assign spi_csn_oe = hostmode ? 1'b1 : 1'b0;
+   assign spi_sd_oe  = hostmode ? 1'b1 : 1'b0;
 
 endmodule // la_spi
