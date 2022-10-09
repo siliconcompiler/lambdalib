004490537dae326f69002774fa65f69eb991a9e5
aolofsson <andreas@zeroasic.com>
2022-10-08 22:50:23 -0400

    Refactoring to improve parameter names

--- stdlib/rtl/la_dsync.v
+++ stdlib/rtl/la_dsync.v
@@ -4,22 +4,23 @@
 //# License:  MIT (see LICENSE file in Lambda repository)                     #
 //#############################################################################
 
-module la_dsync #(parameter PROP = "DEFAULT")   (
+module la_dsync
+  #(parameter PROP = "DEFAULT")   (
     input  clk,    // clock
     input  nreset, // async active low reset
     input  in,     // input data
     output out     // synchronized data
     );
 
-   localparam SYNCPIPE=2;
+   localparam STAGES=2;
 
-   reg [SYNCPIPE-1:0] sync_pipe;
+   reg [STAGES-1:0] shiftreg;
    always @ (posedge clk or negedge nreset)
      if(!nreset)
-       sync_pipe[SYNCPIPE-1:0] <= 'b0;
+       shiftreg[STAGES-1:0] <= 'b0;
      else
-       sync_pipe[SYNCPIPE-1:0] <= {sync_pipe[SYNCPIPE-1:0],in};
+       shiftreg[STAGES-1:0] <= {shiftreg[STAGES-1:0],in};
 
-   assign out = sync_pipe[SYNCPIPE-1];
+   assign out = shiftreg[STAGES-1];
 
 endmodule
--- stdlib/rtl/la_rsync.v
+++ stdlib/rtl/la_rsync.v
@@ -4,20 +4,22 @@
 //# License:   MIT (see LICENSE file in Lambda repository)                    #
 //#############################################################################
 
-module la_rsync #(parameter PROP = "DEFAULT")  (
-   input  clk,
-   input  nrst_in,
-   output nrst_out
+module la_rsync
+  #(parameter PROP = "DEFAULT")
+   (
+    input  clk, // clock
+    input  nrst_in, // async reset input
+    output nrst_out // async assert, sync deassert reset
    );
 
-   localparam SYNCPIPE=2;
+   localparam STAGES=2;
 
-   reg [SYNCPIPE-1:0] sync_pipe;
+   reg [STAGES-1:0] sync_pipe;
    always @ (posedge clk or negedge nrst_in)
      if(!nrst_in)
-       sync_pipe[SYNCPIPE-1:0] <= 'b0;
+       sync_pipe[STAGES-1:0] <= 'b0;
      else
-       sync_pipe[SYNCPIPE-1:0] <= {sync_pipe[SYNCPIPE-2:0],1'b1};
-   assign nrst_out = sync_pipe[SYNCPIPE-1];
+       sync_pipe[STAGES-1:0] <= {sync_pipe[STAGES-2:0],1'b1};
+   assign nrst_out = sync_pipe[STAGES-1];
 
 endmodule
