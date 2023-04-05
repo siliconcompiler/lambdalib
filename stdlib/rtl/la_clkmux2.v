99c3489b21d6b7ce72f8513af9e852e7069d3ea1
aolofsson <andreas@zeroasic.com>
2023-04-05 13:54:29 -0400

    Adding 2 input glitch free clock mux

--- stdlib/rtl/la_clkmux2.v
+++ stdlib/rtl/la_clkmux2.v
@@ -1,17 +1,46 @@
 //#############################################################################
-//# Function: 2:1 Clock Mux                                                   #
-//#############################################################################
-//# Author:   Andreas Olofsson                                                #
-//# License:  MIT (see LICENSE file in Lambda! repository)                        #
+//# Function: 2-Input Glitch-Free Clock Mux                                   #
+//# Copyright: Lambda Project Authors. All rights Reserved.                   #
+//# License:  MIT (see LICENSE file in Lambda repository)                     #
 //#############################################################################
 
-module la_clkmux2 #(parameter PROP = "DEFAULT")   (
+module la_clkmux2
+  #(parameter PROP = "DEFAULT"  // cell property
+    )
+   (
     input  clk0,
     input  clk1,
-    input  sel,
-    output z
+    input  sel0,
+    input  sel1,
+    output out
     );
 
-   assign z = sel ? clk0 : clk1;
+   wire [1:0] maskb;
+   wire [1:0] en;
+   wire [1:0] ensync;
+   wire [1:0] clkg;
 
-endmodule
+   // invert mask (2)
+   la_inv iinv[1:0] (.a(ensync[1:0]), .z(maskb[1:0]));
+
+   // clock enable (2)
+   la_and2 isel[1:0] (.a({sel1,sel0}),
+		      .b(maskb[1:0]),
+		      .z(en[1:0]));
+
+   // synchronizers (2)
+   la_dsync isync[1:0] (.clk({clk1,clk0}),
+			.in(en[1:0]),
+			.out(ensync[1:0]));
+
+   // glith free clock gate (2)
+   la_and2 igate[1:0] (.a({clk1,clk0}),
+		       .b(ensync[1:0]),
+		       .z(clkg[1:0]));
+
+   // final clock or (1)
+   la_or2 iorclk (.a(clkg[0]),
+		  .b(clkg[1]),
+		  .z(out));
+
+endmodule // la_clkmux2
