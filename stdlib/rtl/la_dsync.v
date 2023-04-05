70ac87f0b24d5e98d982e688e80ef06cd2ecca19
aolofsson <andreas@zeroasic.com>
2023-04-05 13:57:04 -0400

    Removing reset from synchronizer - Problem should be solved at source

--- stdlib/rtl/la_dsync.v
+++ stdlib/rtl/la_dsync.v
@@ -5,21 +5,19 @@
 //#############################################################################
 
 module la_dsync
-  #(parameter PROP = "DEFAULT")   (
-    input  clk,    // clock
-    input  nreset, // async active low reset
-    input  in,     // input data
+  #(parameter PROP = "DEFAULT")
+   (
+    input  clk, // clock
+    input  in, // input data
     output out     // synchronized data
     );
 
    localparam STAGES=2;
 
    reg [STAGES-1:0] shiftreg;
-   always @ (posedge clk or negedge nreset)
-     if(!nreset)
-       shiftreg[STAGES-1:0] <= 'b0;
-     else
-       shiftreg[STAGES-1:0] <= {shiftreg[STAGES-2:0],in};
+
+   always @ (posedge clk)
+     shiftreg[STAGES-1:0] <= {shiftreg[STAGES-2:0],in};
 
    assign out = shiftreg[STAGES-1];
 
