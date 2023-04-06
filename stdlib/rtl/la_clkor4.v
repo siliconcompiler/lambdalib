b73c22960763dc7c30c5b31483f6b77de3727fe0
aolofsson <andreas@zeroasic.com>
2023-04-05 21:30:28 -0400

    Adding 4 input clock or gate

---
+++ stdlib/rtl/la_clkor4.v
@@ -0,0 +1,18 @@
+//#############################################################################
+//# Function: 4 Input Clock Or Gate                                           #
+//# Copyright: Lambda Project Authors. All rights Reserved.                   #
+//# License:  MIT (see LICENSE file in Lambda repository)                     #
+//#############################################################################
+
+module la_clkor4 #(parameter PROP = "DEFAULT")
+   (
+    input  a,
+    input  b,
+    input  c,
+    input  d,
+    output z
+    );
+
+   assign z = a | b | c | d;
+
+endmodule
