6a19e17b656cdb6197a70712c445d3470c3ff96c
volkamir <amir@zeroasic.com>
2023-08-18 09:32:54 -0400

    removing YOSIS specific parts, will be handled by sc

--- iolib/rtl/la_ioshort.v
+++ iolib/rtl/la_ioshort.v
@@ -22,9 +22,6 @@ module la_ioshort
    // Using direction to break the loop
    assign a = ~a2b ? b : 1'bz;
    assign b = a2b  ? a : 1'bz;
-`elsif YOSYS
-   assign a = b;
-   assign b = a;
 `else
    // single port pass through short/hack
    la_pt la_pt(a,b);
