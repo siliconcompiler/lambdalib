a98f66e185cfff2edec284f957bc4c0d80d119eb
Andreas Olofsson <andreas@zeroasic.com>
2024-05-03 15:39:57 -0400

    Standardizing around simulation pragma

--- lambdalib/stdlib/rtl/la_dsync.v
+++ lambdalib/stdlib/rtl/la_dsync.v
@@ -4,31 +4,30 @@
 //# License:  MIT (see LICENSE file in Lambda repository)                     #
 //#############################################################################
 
-module la_dsync #(
-    parameter PROP = "DEFAULT"
-) (
-    input  clk,  // clock
-    input  in,   // input data
-    output out   // synchronized data
-);
-
-    localparam STAGES = 2;
-    localparam RND = 1;
+module la_dsync #(parameter PROP = "DEFAULT",
+                  parameter STAGES = 2,         // synchhronizer depth
+                  parameter RND = 1)            // randomize sync
+   (
+    input  clk, // clock
+    input  in,  // input data
+    output out  // synchronized data
+    );
 
     reg     [STAGES:0] shiftreg;
     integer            sync_delay;
 
     always @(posedge clk) begin
         shiftreg[STAGES:0] <= {shiftreg[STAGES-1:0], in};
-`ifndef SYNTHESIS
+`ifdef SIMULATION
         sync_delay <= {$random} % 2;
 `endif
     end
 
-`ifdef SYNTHESIS
-    assign out = shiftreg[STAGES-1];
+`ifdef SIMULATION
+   assign out = (|sync_delay & (|RND)) ? shiftreg[STAGES] : shiftreg[STAGES-1];
+
 `else
-    assign out = (|sync_delay & (|RND)) ? shiftreg[STAGES] : shiftreg[STAGES-1];
+    assign out = shiftreg[STAGES-1];
 `endif
 
 endmodule
