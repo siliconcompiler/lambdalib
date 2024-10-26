a8327c97b7b35bbc71f7dedf753a7c857015a8a3
Andreas Olofsson <andreas@zeroasic.com>
2024-10-25 20:39:41 -0400

    Making input buffer "bullet proof" - For some signals like reset and certain static strap signals, we really can't afford to make a mistake. Since io cell modeling and simulation of tristate io buffer isn't always perfect, we want to make sure that the design is correct by construction as much as possible for these buffers. The bidir signal is very configurable and the cfg bus is generic per library (increasing the chance of a catastrophic mistake). - This input buffer hard codes all choices in teh abstracted library, minimizing the chance that the user/designer makes a mistake. The designer of the lambdapdk still needs to get the signal mapping correct, but this only has to be done once per port, not for every design.

--- lambdalib/iolib/rtl/la_ioinput.v
+++ lambdalib/iolib/rtl/la_ioinput.v
@@ -12,23 +12,23 @@ module la_ioinput
   #(
     parameter PROP = "DEFAULT", // cell property
     parameter SIDE = "NO",      // "NO", "SO", "EA", "WE"
-    parameter CFGW = 16,        // width of core config bus
     parameter RINGW = 8         // width of io ring
     )
    (// io pad signals
-    inout             pad,    // input pad
-    inout             vdd,    // core supply
-    inout             vss,    // core ground
-    inout             vddio,  // io supply
-    inout             vssio,  // io ground
+    inout             pad,   // input pad
+    inout             vdd,   // core supply
+    inout             vss,   // core ground
+    inout             vddio, // io supply
+    inout             vssio, // io ground
     // core facing signals
-    output            z,      // output to core
-    input             ie,     // input enable, 1 = active
-    inout [RINGW-1:0] ioring, // generic ioring interface
-    input [CFGW-1:0]  cfg     // generic config interface
+    output            z,     // output to core
+    inout [RINGW-1:0] ioring // generic ioring interface
     );
 
-   // to core
-   assign z = ie ? pad : 1'b0;
+   assign z = pad;
+
+`ifndef VERILATOR
+   rnmos #1 (pad, vssio, 1'b1); // weak pulldown
+`endif
 
 endmodule
