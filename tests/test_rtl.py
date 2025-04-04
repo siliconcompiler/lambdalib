ba7bb6ef1524dfa5257fc30d9d544ff0eac63461
Peter Gadfort <gadfort@zeroasic.com>
2025-04-04 15:01:20 -0400

    dont import lambdalib

--- tests/test_rtl.py
+++ tests/test_rtl.py
@@ -1,8 +1,7 @@
 import pytest
 
-import lambdalib.ramlib.tests.tb_la_asyncfifo as ramlib_tests
-
 
 @pytest.mark.eda
 def test_la_asyncfifo():
+    import lambdalib.ramlib.tests.tb_la_asyncfifo as ramlib_tests
     ramlib_tests.test_la_asyncfifo()
