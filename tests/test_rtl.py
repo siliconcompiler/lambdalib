57a7769b1f78606bba086da6e5d58823c045b8f8
Peter Gadfort <gadfort@zeroasic.com>
2025-04-04 15:01:20 -0400

    dont import lambdalib

---
+++ tests/test_rtl.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.mark.eda
+def test_dummy():
+    assert True
--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,49 +1,55 @@
-# import pytest
-# from siliconcompiler import Chip
-
-# from lambdalib import \
-#     auxlib, \
-#     fpgalib, \
-#     iolib, \
-#     padring, \
-#     ramlib, \
-#     stdlib, \
-#     vectorlib
-# libraries = [
-#     auxlib,
-#     fpgalib,
-#     iolib,
-#     padring,
-#     ramlib,
-#     stdlib,
-#     vectorlib
-# ]
+import pytest
+from siliconcompiler import Chip
 
 
-# @pytest.mark.parametrize('lib', libraries)
-# def test_setup(lib):
-#     chip = Chip('<lib>')
-#     chip.use(lib)
+@pytest.fixture(params=(1, 2, 3, 4, 5, 6, 7))
+def lib(request):
+    from lambdalib import \
+        auxlib, \
+        fpgalib, \
+        iolib, \
+        padring, \
+        ramlib, \
+        stdlib, \
+        vectorlib
 
-#     lib_name = lib.__name__.replace('.', '_')
+    if request.param == 1:
+        return auxlib
+    if request.param == 2:
+        return fpgalib
+    if request.param == 3:
+        return iolib
+    if request.param == 4:
+        return padring
+    if request.param == 5:
+        return ramlib
+    if request.param == 6:
+        return stdlib
+    if request.param == 7:
+        return vectorlib
 
-#     assert lib_name in chip.getkeys('library')
-#     assert len(chip.get('library', lib_name, 'option', 'ydir')) == 1
+
+def test_setup(lib):
+    chip = Chip('<lib>')
+    chip.use(lib)
+
+    lib_name = lib.__name__.replace('.', '_')
+
+    assert lib_name in chip.getkeys('library')
+    assert len(chip.get('library', lib_name, 'option', 'ydir')) == 1
 
 
-# @pytest.mark.parametrize(
-#         'lib,has_idir',
-#         [(lib, lib == padring) for lib in libraries])
-# def test_setup_with_idir(lib, has_idir):
-#     chip = Chip('<lib>')
-#     chip.use(lib)
+def test_setup_with_idir(lib):
+    from lambdalib import padring
+    chip = Chip('<lib>')
+    chip.use(lib)
 
-#     lib_name = lib.__name__.replace('.', '_')
+    lib_name = lib.__name__.replace('.', '_')
 
-#     assert lib_name in chip.getkeys('library')
+    assert lib_name in chip.getkeys('library')
 
-#     excpect_idir = 0
-#     if has_idir:
-#         excpect_idir = 1
+    excpect_idir = 0
+    if lib == padring:
+        excpect_idir = 1
 
-#     assert len(chip.get('library', lib_name, 'option', 'idir')) == excpect_idir
+    assert len(chip.get('library', lib_name, 'option', 'idir')) == excpect_idir
