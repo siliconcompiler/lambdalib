e77429d9c08c77f236d0daddddc1b386e903c45a
Andreas Olofsson <andreas@zeroasic.com>
2025-07-15 07:50:05 -0400

    Adding more tests

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,5 +1,15 @@
+from lambdalib import stdlib
 from lambdalib import auxlib
+from lambdalib import ramlib
+from lambdalib import vectorlib
 from lambdalib import iolib
+from lambdalib import fpgalib
+
+def test_class_stdlib():
+    for name in stdlib.__all__:
+        obj = getattr(stdlib, name)
+        instance = obj()
+        assert instance.check_filepaths()
 
 def test_class_auxlib():
     for name in auxlib.__all__:
@@ -7,8 +17,26 @@ def test_class_auxlib():
         instance = obj()
         assert instance.check_filepaths()
 
+def test_class_ramlib():
+    for name in ramlib.__all__:
+        obj = getattr(ramlib, name)
+        instance = obj()
+        assert instance.check_filepaths()
+
+def test_class_vectorlib():
+    for name in vectorlib.__all__:
+        obj = getattr(vectorlib, name)
+        instance = obj()
+        assert instance.check_filepaths()
+
 def test_class_iolib():
     for name in iolib.__all__:
         obj = getattr(iolib, name)
+        instance = obj()
+        assert instance.check_filepaths()
+
+def test_class_fpgalib():
+    for name in fpgalib.__all__:
+        obj = getattr(fpgalib, name)
         instance = obj()
         assert instance.check_filepaths()
