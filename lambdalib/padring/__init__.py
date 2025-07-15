18768154b4c6f2dae7e005ae463fa8b0f6dc6bb4
Andreas Olofsson <andreas@zeroasic.com>
2025-07-15 07:50:49 -0400

    Moving padring files to iolib - With new separation, there should be no need for extra directory structure, iolib and padring are tightly coupled

--- lambdalib/padring/__init__.py
+++ lambdalib/padring/__init__.py
@@ -1,11 +1,10 @@
+########################################
+# Old SiliconCompiler Setup (deprecated)
+########################################
 from siliconcompiler import Library
 from lambdalib._common import register_data_source
 from lambdalib import iolib
 
-
-########################
-# SiliconCompiler Setup
-########################
 def setup():
     '''
     Lambdalib padring
@@ -14,8 +13,13 @@ def setup():
     lib = Library('lambdalib_padring', package='lambdalib', auto_enable=True)
     register_data_source(lib)
 
-    lib.add('option', 'idir', "padring/rtl")
-    lib.add('option', 'ydir', "padring/rtl")
+    cells = ['la_ioside',
+             'la_iopadring']
+
+    for item in cells:
+        lib.input(f'{item}/rtl/{item}.v')
+
+    lib.add('option', 'idir', "padring/include")
 
     lib.use(iolib)
 
