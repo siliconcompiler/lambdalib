5fb47bfa5fe2b623cbaaa9c00ce97b853ad2f069
Andreas Olofsson <andreas@zeroasic.com>
2025-07-15 07:50:33 -0400

    Adding single file setup code

--- lambdalib/_common.py
+++ lambdalib/_common.py
@@ -3,3 +3,20 @@ _version = "0.3.4"
 
 def register_data_source(chip):
     chip.register_source("lambdalib", "python://lambdalib")
+
+
+def basic_setup(design, path, name):
+    topmodule = name
+    datadir_name = f'{name}_data'
+    source = [f'rtl/{name}.v']
+
+    # set data home directory
+    design.register_datadir(datadir_name, path)
+
+    # rtl files
+    fileset = 'rtl'
+    for item in source:
+        design.add_file(item, fileset, dataref=datadir_name)
+
+    # top module
+    design.set_topmodule(topmodule, fileset)
