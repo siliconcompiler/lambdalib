e878af9e50a4b321d2d8903e0e7026b9a996f833
Andreas Olofsson <andreas@zeroasic.com>
2025-07-16 20:54:31 -0400

    Moving away from flattening

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,48 +1,43 @@
-from lambdalib import stdlib
-from lambdalib import auxlib
-from lambdalib import ramlib
-from lambdalib import vectorlib
-from lambdalib import iolib
-from lambdalib import fpgalib
+import lambdalib as ll
 
 
 def test_class_stdlib():
-    for name in stdlib.__all__:
-        obj = getattr(stdlib, name)
+    for name in ll.stdlib.__all__:
+        obj = getattr(ll.stdlib, name)
         instance = obj()
         assert instance.check_filepaths()
 
 
 def test_class_auxlib():
-    for name in auxlib.__all__:
-        obj = getattr(auxlib, name)
+    for name in ll.auxlib.__all__:
+        obj = getattr(ll.auxlib, name)
         instance = obj()
         assert instance.check_filepaths()
 
 
 def test_class_ramlib():
-    for name in ramlib.__all__:
-        obj = getattr(ramlib, name)
+    for name in ll.ramlib.__all__:
+        obj = getattr(ll.ramlib, name)
         instance = obj()
         assert instance.check_filepaths()
 
 
 def test_class_vectorlib():
-    for name in vectorlib.__all__:
-        obj = getattr(vectorlib, name)
+    for name in ll.vectorlib.__all__:
+        obj = getattr(ll.vectorlib, name)
         instance = obj()
         assert instance.check_filepaths()
 
 
 def test_class_iolib():
-    for name in iolib.__all__:
-        obj = getattr(iolib, name)
+    for name in ll.iolib.__all__:
+        obj = getattr(ll.iolib, name)
         instance = obj()
         assert instance.check_filepaths()
 
 
 def test_class_fpgalib():
-    for name in fpgalib.__all__:
-        obj = getattr(fpgalib, name)
+    for name in ll.fpgalib.__all__:
+        obj = getattr(ll.fpgalib, name)
         instance = obj()
         assert instance.check_filepaths()
