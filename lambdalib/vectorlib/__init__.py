56197613189a7e45bf40f511bfec4805ae894969
Andreas Olofsson <andreas@zeroasic.com>
2025-07-16 22:23:17 -0400

    Updating package

--- lambdalib/vectorlib/__init__.py
+++ lambdalib/vectorlib/__init__.py
@@ -1,7 +1,5 @@
 from .la_vbuf.la_vbuf import vbuf
 from .la_vinv.la_vinv import vinv
-from .la_visohi.la_visohi import visohi
-from .la_visolo.la_visolo import visolo
 from .la_vmux.la_vmux import vmux
 from .la_vmux2.la_vmux2 import vmux2
 from .la_vmux3.la_vmux3 import vmux3
@@ -13,8 +11,6 @@ from .la_vmux8.la_vmux8 import vmux8
 
 __all__ = ['vbuf',
            'vinv',
-           'visohi',
-           'visolo',
            'vmux',
            'vmux2',
            'vmux3',
