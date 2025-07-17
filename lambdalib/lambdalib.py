60617f14b3e6b2f54345a3dc8eddcdd32d9b39df
Andreas Olofsson <andreas@zeroasic.com>
2025-07-17 10:51:54 -0400

    Adding 'la_' prefix to topmodule name by convention -Users should not have to reference 'la_' when programming in pythong, this is more typing imposed by the flat name space of verilog. Shouldn't burden people with it.

--- lambdalib/lambdalib.py
+++ lambdalib/lambdalib.py
@@ -2,20 +2,22 @@ from typing import List, Union
 from pathlib import Path
 from siliconcompiler import DesignSchema
 
+
 class Lambda(DesignSchema):
 
     def __init__(self,
                  name: str,
                  sources: List[str],
-                 path: Union[str,Path]):
+                 path: Union[str, Path]):
 
         super().__init__(name)
 
         fileset = 'rtl'
         dataroot = f'{name}'
+        topmodule = f'la_{name}'
 
         self.set_dataroot(dataroot, path)
-        self.set_topmodule(name, fileset)
+        self.set_topmodule(topmodule, fileset)
 
         for item in sources:
             self.add_file(item, fileset, dataroot=dataroot)
