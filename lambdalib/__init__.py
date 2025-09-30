deac7318671c4871e12a3fb8b473b29f7a2753a0
Peter <46405338+gadfort@users.noreply.github.com>
2025-09-30 09:50:55 -0400

    move to rc3

--- lambdalib/__init__.py
+++ lambdalib/__init__.py
@@ -12,7 +12,7 @@ from lambdalib import stdlib
 from lambdalib import ramlib
 from lambdalib import veclib
 
-__version__ = "0.4.0-rc2"
+__version__ = "0.4.0-rc3"
 
 
 class LambalibTechLibrary(Design):
