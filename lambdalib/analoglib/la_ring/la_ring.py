e5795e676682e3048c3143a97ce0537a68241fc2
Andreas Olofsson <andreas@zeroasic.com>
2025-12-12 17:16:07 -0500

    Adding ability to drive different types of ring oscillators

--- lambdalib/analoglib/la_ring/la_ring.py
+++ lambdalib/analoglib/la_ring/la_ring.py
@@ -1,6 +1,12 @@
 from lambdalib.lambdalib import Lambda
 from lambdalib.stdlib import Inv
+from lambdalib.stdlib import Buf
 from lambdalib.stdlib import Nand2
+from lambdalib.stdlib import Nand3
+from lambdalib.stdlib import Nand4
+from lambdalib.stdlib import Nor2
+from lambdalib.stdlib import Nor3
+from lambdalib.stdlib import Nor4
 
 
 class Ring(Lambda):
@@ -10,7 +16,13 @@ class Ring(Lambda):
 
         # deps
         self.add_depfileset(Inv(), depfileset='rtl', fileset='rtl')
+        self.add_depfileset(Buf(), depfileset='rtl', fileset='rtl')
         self.add_depfileset(Nand2(), depfileset='rtl', fileset='rtl')
+        self.add_depfileset(Nand3(), depfileset='rtl', fileset='rtl')
+        self.add_depfileset(Nand4(), depfileset='rtl', fileset='rtl')
+        self.add_depfileset(Nor2(), depfileset='rtl', fileset='rtl')
+        self.add_depfileset(Nor3(), depfileset='rtl', fileset='rtl')
+        self.add_depfileset(Nor4(), depfileset='rtl', fileset='rtl')
 
 
 if __name__ == "__main__":
