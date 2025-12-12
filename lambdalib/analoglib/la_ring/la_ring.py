4df0becfa7722fcae065ddc94f466536d77bff85
Andreas Olofsson <andreas@zeroasic.com>
2025-12-12 15:18:11 -0500

    Fixing ring oscillator setup files

--- lambdalib/analoglib/la_ring/la_ring.py
+++ lambdalib/analoglib/la_ring/la_ring.py
@@ -1,12 +1,17 @@
 from lambdalib.lambdalib import Lambda
+from lambdalib.stdlib import Inv
+from lambdalib.stdlib import Nand2
 
-
-class PLL(Lambda):
+class Ring(Lambda):
     def __init__(self):
-        name = 'la_pll'
+        name = 'la_ring'
         super().__init__(name, __file__)
+
+        # deps
+        self.add_depfileset(Inv(), depfileset='rtl', fileset='rtl')
+        self.add_depfileset(Nand2(), depfileset='rtl', fileset='rtl')
 
 
 if __name__ == "__main__":
-    d = PLL()
+    d = Ring()
     d.write_fileset(f"{d.name}.f", fileset="rtl")
