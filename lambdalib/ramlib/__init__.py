0dac4312f574a497dc1500ce7c20373aae8162f1
Peter Gadfort <gadfort@zeroasic.com>
2026-01-08 10:30:47 -0500

    add spregfile

--- lambdalib/ramlib/__init__.py
+++ lambdalib/ramlib/__init__.py
@@ -4,8 +4,9 @@ from siliconcompiler import Design
 
 from .la_asyncfifo.la_asyncfifo import Asyncfifo
 from .la_syncfifo.la_syncfifo import Syncfifo
-from .la_dpram.la_dpram import Dpram
 from .la_spram.la_spram import Spram
+from .la_spregfile.la_spregfile import Spregfile
+from .la_dpram.la_dpram import Dpram
 from .la_tdpram.la_tdpram import Tdpram
 
 __all__ = ['Asyncfifo',
@@ -23,8 +24,9 @@ class RAMLib(Design):
         with self.active_fileset("rtl"):
             self.add_depfileset(Asyncfifo(), depfileset="rtl")
             self.add_depfileset(Syncfifo(), depfileset="rtl")
-            self.add_depfileset(Dpram(), depfileset="rtl")
             self.add_depfileset(Spram(), depfileset="rtl")
+            self.add_depfileset(Spregfile(), depfileset="rtl")
+            self.add_depfileset(Dpram(), depfileset="rtl")
             self.add_depfileset(Tdpram(), depfileset="rtl")
 
 
