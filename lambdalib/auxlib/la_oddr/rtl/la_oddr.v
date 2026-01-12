c8dfe4b64efa511d7fac3f2068f53bf0fbaabcce
Andreas Olofsson <andreas@zeroasic.com>
2026-01-12 13:03:04 -0500

    Simplifying circuit - No latches, flip flops only!

--- lambdalib/auxlib/la_oddr/rtl/la_oddr.v
+++ lambdalib/auxlib/la_oddr/rtl/la_oddr.v
@@ -6,72 +6,47 @@
 //
 // Supports the following operating modes
 // 0 - bypass
-// 1 - oppositive-edge
-// 2 - same-edge
+// 1 - sample (negedge data arrieves before posedge data, experimental)
+// 2 - align (standard, in0, in1 arrives at same clock cycle)
 // 3 - n/a
 //
-// NOTE: hold latch used for negedge sampling in place of negedge sampling
-//       to improve dity cycle resilence.
+// NOTE:
+// - external to this block, a forward-shifted clock must be provided that
+//   is forward shifted by 90 degrees from this clock
+// - See docs/oiddr_waveform.json for waveform description
 //
 //#############################################################################
 
-module la_oddr #(parameter PROP = "LATCH"
+module la_oddr #(parameter PROP = ""
                  )
    (
     input       clk,  // clock input
     input [1:0] mode, // operating modes
     input       in0,  // data for rising edge
     input       in1,  // data for falling edge
-    output      out   // dual data rate output
+    output      q     // dual data rate output
     );
 
-   //###################################
-   // Declarations
-   //###################################
-
    reg  q0;            // Sampled rising edge data
-   reg  q1_low;        // For Mode 1 (Opposite-Edge)
-   reg  q1_high;       // For Mode 2 (Same-Edge)
-   wire q1_sh;         // Final ddata for negedge
+   reg  q1;            // Alignment for negedge
+   reg  q1_negedge;    // Negedge data
    wire ddr;           // Dual data rate (pre bypass mux)
 
-   //###################################
-   // Primary Capture (Latch or FF)
-   //###################################
-
+   // sampling inputs
    always @(posedge clk)
-     q0 <= in0;
+     begin
+        q0 <= in0;
+        q1 <= in1;
+     end
 
-   //###################################
-   // Secondary Capture (Latch or FF)
-   //###################################
+   // mode based negedge sample
+   always @(negedge clk)
+     q1_negedge <= (mode == 2) ? q1 : in1;
 
-   generate
-      if (PROP == "LATCH") begin : g_latch
-`ifdef VERILATOR
-         // In Verilator, we mimic the latch behavior with FFs
-         wire clk_sample = (mode == 1) ? !clk : clk;
-         reg  q1_sim;
-         always @(posedge clk_sample)
-           q1_sim <= in1;
-         assign q1_sh = q1_sim;
-`else
-         always @(clk or in1) if (clk)  q1_high <= in1; // high latch
-         always @(clk or in1) if (!clk) q1_low  <= in1; // low latch
-         assign q1_sh = (mode == 1) ? q1_low : q1_high; // edge/mode selector
-`endif
-      end else begin : g_ff
-         always @(posedge clk) q1_high <= in1;
-         always @(negedge clk) q1_low  <= in1;
-         assign q1_sh = (mode == 1) ? q1_low : q1_high;
-      end
-   endgenerate
+   // use clk as mux select
+   assign ddr = clk ? q0 : q1_negedge;
 
-   //###################################
-   // Output muxing
-   //###################################
-
-   assign ddr = clk ? q0 : q1_sh;
-   assign out = (mode == 0) ? in0 : ddr;
+   // fast async bypass
+   assign q = (mode == 0) ? in0 : ddr;
 
 endmodule
