16d5066c475a3aed0fd1d19360e4cd4791c834e3
Rice Shelley <rice.shelley@ieee.org>
2026-01-19 15:36:51 -0500

    RSync test rework

--- lambdalib/reusable_tests/auxlib/la_rsync/la_rsync_test.py
+++ lambdalib/reusable_tests/auxlib/la_rsync/la_rsync_test.py
@@ -1,8 +1,7 @@
 try:
     import cocotb
 
-    from cocotb.clock import Clock
-    from cocotb.triggers import Timer, RisingEdge, ReadOnly
+    from cocotb.triggers import Timer, ValueChange
     from lambdalib.reusable_tests.cocotb_common import run_cocotb
     _has_cocotb = True
 except ModuleNotFoundError:
@@ -13,32 +12,43 @@ from lambdalib.auxlib import Rsync
 
 
 if _has_cocotb:
+
+    async def drive_clock(clk, period_ns, n=1):
+        half_period = period_ns / 2
+        for i in range(n):
+            clk.value = 0
+            await Timer(half_period, unit="ns")
+            clk.value = 1
+            await Timer(half_period, unit="ns")
+
     @cocotb.test()
     async def test_la_rsync_basic(dut):
         """Test basic asynchronous assertion and synchronous de-assertion"""
 
-        clk_period_ns = 10
         STAGES = int(dut.STAGES.value)
+        clk_period_ns = 10
 
         nrst_in = dut.nrst_in
         nrst_out = dut.nrst_out
+        clk = dut.clk
+
+        # Drive clock signal
+        clk.value = 0
 
         # De-assert reset during start of test
         nrst_in.value = 1
 
-        # Start clock
-        Clock(dut.clk, clk_period_ns, unit="ns").start()
+        await Timer(clk_period_ns*2, unit="ns")
 
         ####################################
         # 1. Trigger Asynchronous Reset
         ####################################
-        await RisingEdge(dut.clk)
-        # Assert reset offset from clock edge
-        await Timer(clk_period_ns/2, unit="ns")
+
+        # Assert reset
         nrst_in.value = 0
 
-        # Check for immediate (asynchronous) assertion
-        await Timer(1, unit="step")
+        # Check for asynchronous assertion
+        await ValueChange(nrst_out)
         assert nrst_out.value == 0, "Reset output did not assert immediately!"
 
         ####################################
@@ -46,11 +56,11 @@ if _has_cocotb:
         ####################################
         await Timer(clk_period_ns*2, unit="ns")
         nrst_in.value = 1
+        await Timer(clk_period_ns*2, unit="ns")
 
         # Wait for the synchronizer pipeline to clear.
         cycle = 0
         while True:
-            await ReadOnly()
             # Check if it de-asserts too early
             if cycle < STAGES:
                 assert nrst_out.value == 0, f"Reset de-asserted too early at cycle {cycle}"
@@ -58,10 +68,9 @@ if _has_cocotb:
                 assert nrst_out.value == 1, f"Reset failed to de-assert {cycle}"
                 break
 
-            await RisingEdge(dut.clk)
+            await drive_clock(clk, clk_period_ns)
             cycle += 1
 
-        await RisingEdge(dut.clk)
 else:
     def test_la_rsync_basic():
         """Placeholder test when cocotb is not installed."""
