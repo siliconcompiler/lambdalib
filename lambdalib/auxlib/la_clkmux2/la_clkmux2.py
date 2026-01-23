f1f0f06ba53b21b443b7cb60a649330d95aea259
Andreas Olofsson <andreas@zeroasic.com>
2026-01-23 08:26:32 -0500

    Adding testbench for glitch free clock mux

--- lambdalib/auxlib/la_clkmux2/la_clkmux2.py
+++ lambdalib/auxlib/la_clkmux2/la_clkmux2.py
@@ -1,5 +1,5 @@
 from lambdalib.lambdalib import Lambda
-
+from siliconcompiler import Design
 from lambdalib.stdlib import Inv
 from lambdalib.stdlib import And2
 from lambdalib.stdlib import Clkor2
@@ -13,13 +13,24 @@ class Clkmux2(Lambda):
         super().__init__(name, __file__)
 
         # dependencies
-        self.add_depfileset(Inv(), depfileset='rtl', fileset='rtl')
-        self.add_depfileset(And2(), depfileset='rtl', fileset='rtl')
-        self.add_depfileset(Clkor2(), depfileset='rtl', fileset='rtl')
-        self.add_depfileset(Drsync(), depfileset='rtl', fileset='rtl')
-        self.add_depfileset(Clkicgand(), depfileset='rtl', fileset='rtl')
+        deps = [Inv(), And2(), Clkor2(), Drsync(), Clkicgand()]
+        with self.active_fileset('rtl'):
+            for item in deps:
+                self.add_depfileset(item)
 
+class Clkmux2TB(Design):
+    def __init__(self):
+        name = 'tb_la_clkmux2'
+        super().__init__(name)
+
+        # dependencies
+        self.set_dataroot(name, __file__)
+        with self.active_fileset('rtl'):
+            self.add_file(f"testbench/{name}.v")
+            self.add_depfileset(Clkmux2())
 
 if __name__ == "__main__":
     d = Clkmux2()
+    d.write_fileset(f"{d.name}.f", fileset="rtl")
+    d = Clkmux2TB()
     d.write_fileset(f"{d.name}.f", fileset="rtl")
