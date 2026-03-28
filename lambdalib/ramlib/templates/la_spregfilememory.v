42e98b206bd061058622d2d56f066e033ad9eef5
Andreas Olofsson <andreas@zeroasic.com>
2026-03-28 07:39:51 -0400

    Updating template libraries to match rtl for ramlib

--- lambdalib/ramlib/templates/la_spregfilememory.v
+++ lambdalib/ramlib/templates/la_spregfilememory.v
@@ -8,7 +8,7 @@
  * This is a wrapper for selecting from a set of hardened memory macros.
  *
  * A synthesizable reference model is used when the PROP is DEFAULT. The
- * synthesizable model does not implement the cfg and test interface and should
+ * synthesizable model does not implement the ctrl interface and should
  * only be used for basic testing and for synthesizing for FPGA devices.
  * Advanced ASIC development should rely on complete functional models
  * supplied on a per macro basis.
@@ -22,11 +22,11 @@
 
 (* keep_hierarchy *)
 module {{ type }}
-  #(parameter DW     = 32,          // Memory width
-    parameter AW     = 10,          // Address width (derived)
-    parameter PROP   = "DEFAULT",   // Pass through variable for hard macro
-    parameter CTRLW  = 128,         // Width of asic ctrl interface
-    parameter TESTW  = 128          // Width of asic test interface
+  #(parameter DW      = 32,         // Memory width
+    parameter AW      = 10,         // Address width (derived)
+    parameter PROP    = "DEFAULT",  // Pass through variable for hard macro
+    parameter CTRLW   = 32,         // Width of ctrl interface
+    parameter STATUSW = 32          // Width of status interface
     )
    (// Memory interface
     input clk, // write clock
@@ -36,13 +36,10 @@ module {{ type }}
     input [AW-1:0] addr, //write address
     input [DW-1:0] din, //write data
     output [DW-1:0] dout, //read output data
-    // Power signals
-    input vss, // ground signal
-    input vdd, // memory core array power
-    input vddio, // periphery/io power
-    // Generic interfaces
-    input [CTRLW-1:0] ctrl, // pass through ASIC control interface
-    input [TESTW-1:0] test // pass through ASIC test interface
+    // Technology interfaces
+    input selctrl, // selects control interface
+    input [CTRLW-1:0] ctrl, // pass through control interface
+    output [STATUSW-1:0] status // pass through status interface
     );
 
     // Total number of bits
@@ -55,7 +52,7 @@ module {{ type }}
     localparam MEM_WIDTH = {% for memory, width in width_table %}
       (MEM_PROP == "{{ memory }}") ? {{ width }} :{% endfor %}
       0;
- 
+
     localparam MEM_DEPTH = {% for memory, depth in depth_table %}
       (MEM_PROP == "{{ memory }}") ? {{ depth }} :{% endfor %}
       0;
@@ -67,7 +64,7 @@ module {{ type }}
             .AW(AW),
             .PROP(PROP),
             .CTRLW(CTRLW),
-            .TESTW(TESTW)
+            .STATUSW(STATUSW)
         ) memory(
             .clk(clk),
             .ce(ce),
@@ -76,11 +73,9 @@ module {{ type }}
             .addr(addr),
             .din(din),
             .dout(dout),
-            .vss(vss),
-            .vdd(vdd),
-            .vddio(vddio),
+            .selctrl(selctrl),
             .ctrl(ctrl),
-            .test(test)
+            .status(status)
         );
       end
       if (MEM_PROP != "SOFT") begin: itech
