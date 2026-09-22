e41c9f5b4d7ec70897a721b412fb330d5cdee2a5
Andreas Olofsson <andreas@zeroasic.com>
2026-09-22 19:57:29 -0400

    Better documentation of cut cell

--- lambdalib/iolib/la_iocut/rtl/la_iocut.v
+++ lambdalib/iolib/la_iocut/rtl/la_iocut.v
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Function: Supply Ring Cut IO Cell
+ * Function: Supply Ring Cut IO Cell (propagates vss only)
  * Copyright: Lambda Project Authors. All rights Reserved.
  * License:  MIT (see LICENSE file in Lambda repository)
  *
@@ -15,17 +15,17 @@ module la_iocut
     parameter RINGW = 8         // width of io ring
     )
    (
-    // ground never cut
-    inout             vss,   // core ground
-    // cut these
-    inout             vdd0,   // core supply from section before
-    inout             vdd1,   // core supply from next section
-    inout             vddio0, // io supply from section before
-    inout             vddio1, // io supply from next section
-    inout             vssio0, // io ground from section before
-    inout             vssio1, // io ground from next section
-    inout [RINGW-1:0] ioring0,// generic ioring interface from section before
-    inout [RINGW-1:0] ioring1 // generic ioring interface from next section
+    // shorted signals
+    inout             vss,     // core ground
+    // cut signals
+    inout             vdd0,    // core supply from section before
+    inout             vdd1,    // core supply from next section
+    inout             vddio0,  // io supply from section before
+    inout             vddio1,  // io supply from next section
+    inout             vssio0,  // io ground from section before
+    inout             vssio1,  // io ground from next section
+    inout [RINGW-1:0] ioring0, // generic ioring interface from section before
+    inout [RINGW-1:0] ioring1  // generic ioring interface from next section
     );
 
 endmodule
